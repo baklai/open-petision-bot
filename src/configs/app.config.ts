7528448988b20ef8b496b1cf2189192a07e4c205
Dmitrii <d.n.baklai@gmail.com>
2024-07-19 21:25:07 +0300

    fix: edit config

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,17 +1,15 @@
 const PORT = '3000';
 const HOST = '0.0.0.0';
 const SECRET = '4EfVKy5nXIXgD6fNk2ssIKy5nXIXgD6fNkfVKy5n';
-const DONATE = null;
-const MONGO_URI = null;
 
 export default () => ({
   HOST: process.env.HOST || HOST,
   PORT: parseInt(process.env.PORT || PORT, 10),
   NODE_ENV: process.env.NODE_ENV || 'production',
   SECRET: process.env.SECRET || SECRET,
-  DONATE: process.env.DONATE || DONATE,
-  MONGO_URI: process.env.MONGO_URI || MONGO_URI,
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  WEB_HOOK: process.env.WEB_HOOK,
+  DONATE: process.env.DONATE || null,
+  MONGO_URI: process.env.MONGO_URI || null,
+  BOT_TOKEN: process.env.BOT_TOKEN || null,
+  WEB_HOOK: process.env.WEB_HOOK || null,
   PROXY: process.env.PROXY || null
 });
